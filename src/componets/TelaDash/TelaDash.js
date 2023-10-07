6cdcabd04dd100665852228634d698037e449aaa
saulvargas88 <saulvargascosta@hotmail.com>
2023-10-07 13:26:00 -0300

    css

--- src/componets/TelaDash/TelaDash.js
+++ src/componets/TelaDash/TelaDash.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './TelaDash.css'; // Importe o arquivo CSS aqui
 
 
 
 export const TelaDash = () => {
   return (
-    <div style={styled.container}>
-      <h1>
+    <div style={styles.container}>
+      <h1 style={{color: 'white', fontWeight: 'bold'}}>
         Tela de DashBoArd
       </h1>
-      <Link to="/">Retornar a Página de Login</Link>
+      <Link style={{color: 'green', fontWeight: 'bold'}} to="/">Retornar a Página de Login</Link>
+      <div className="container2"> 
+        <p>AQUI NOVO STYLE</p>
+      </div>
     </div>
+    
   )
 }
+
+const styles = {
+  container: {
+    backgroundColor: 'blue',  },
+};
